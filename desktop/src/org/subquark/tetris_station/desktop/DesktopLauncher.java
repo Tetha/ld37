f5f0f7a432eeb7984f90c64ff70e454edef6fab6
Harald Krämer <tetha42@gmail.com>
2016-12-10 10:17:01 +0100

    init

---
+++ desktop/src/org/subquark/tetris_station/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package org.subquark.tetris_station.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import org.subquark.tetris_station.TetrisSpaceStation;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new TetrisSpaceStation(), config);
+	}
+}
