07412394adfb13624076f70ced8da1e2e01e4007
Harald Krämer <tetha42@gmail.com>
2016-12-11 15:45:35 +0100

    remved some stray sysout

--- core/src/org/subquark/tetris_station/activation/actions/ApplyDamageFromSun.java
+++ core/src/org/subquark/tetris_station/activation/actions/ApplyDamageFromSun.java
@@ -7,7 +7,6 @@ public class ApplyDamageFromSun implements PostActivationAction {
 
     @Override
     public void act(GameState gs) {
-        System.out.println(String.format("Checking: %d > %d", gs.shipPosition, GameConstants.SUN_DISTANCE_THRESHOLD));
         if (gs.shipPosition >= GameConstants.SUN_DISTANCE_THRESHOLD) {
             gs.health = Math.max(0, gs.health - 1);
         }
