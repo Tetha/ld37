f09d89196294b472e3f4e5e1893391c3cb5bcfbe
Harald Krämer <tetha42@gmail.com>
2016-12-11 12:58:19 +0100

    Added cards to build all the rooms

--- core/src/org/subquark/tetris_station/cards/Card.java
+++ core/src/org/subquark/tetris_station/cards/Card.java
@@ -1,6 +1,11 @@
 package org.subquark.tetris_station.cards;
 
+import org.subquark.tetris_station.cards.actions.BuildDefenseGun;
+import org.subquark.tetris_station.cards.actions.BuildEnergyTransmitter1;
+import org.subquark.tetris_station.cards.actions.BuildEnergyTransmitter2;
 import org.subquark.tetris_station.cards.actions.BuildEngineAction;
+import org.subquark.tetris_station.cards.actions.BuildMetalGenerator;
+import org.subquark.tetris_station.cards.actions.BuildWarpCore;
 import org.subquark.tetris_station.cards.actions.CardAction;
 
 public class Card {
@@ -8,10 +13,45 @@ public class Card {
     public String description;
     public CardAction playAction;
     
+    public static Card createBuildDefenseGun() {
+        Card result = new Card();
+        result.headline = "Build Defense Gun";
+        result.playAction = new BuildDefenseGun();
+        return result;
+    }
+
+    public static Card createBuildEnergyTransmitter1() {
+        Card result = new Card();
+        result.headline = "Build Horizontal Energy Transmitter";
+        result.playAction = new BuildEnergyTransmitter1();
+        return result;
+    }
+    
+    public static Card createBuildEnergyTransmitter2() {
+        Card result = new Card();
+        result.headline = "Build Vertical Energy Transmitter";
+        result.playAction = new BuildEnergyTransmitter2();
+        return result;
+    }
+    
     public static Card createBuildEngineCard() {
         Card result = new Card();
-        result.headline = "Buid Engine";
+        result.headline = "Build Engine";
         result.playAction = new BuildEngineAction();
+        return result;
+    }
+    
+    public static Card createBuildMetalGenerator() {
+        Card result = new Card();
+        result.headline = "Build Metal Generator";
+        result.playAction = new BuildMetalGenerator();
+        return result;
+    }
+    
+    public static Card createWarpCore() {
+        Card result = new Card();
+        result.headline = "Build Warp Core";
+        result.playAction = new BuildWarpCore();
         return result;
     }
 }
