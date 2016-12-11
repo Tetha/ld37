974fa154acfe1a38090ede8ad1bd5692de957c42
Harald Krämer <tetha42@gmail.com>
2016-12-11 10:16:02 +0100

    Implemented room adjacency check

--- core/src/org/subquark/tetris_station/rooms/RoomGrid.java
+++ core/src/org/subquark/tetris_station/rooms/RoomGrid.java
@@ -1,7 +1,12 @@
 package org.subquark.tetris_station.rooms;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.List;
+import java.util.Map;
+import java.util.Set;
+import java.util.stream.Collectors;
 
 import org.subquark.tetris_station.GameConstants;
 
@@ -11,13 +16,22 @@ public class RoomGrid {
     private List<Room> rooms = new ArrayList<Room>();
     private Group actorGroup;
     private RoomGridDisplay display;
-    private boolean[][] isOccupied;
+    private int[][] isOccupied;
     
+    private Map<Integer, Set<Integer>> adjacencies = new HashMap<>();
+    
+    private static final int NO_ROOM = -1;
     public RoomGrid(Group actorGroup) {
         this.actorGroup = actorGroup;
         this.display = new RoomGridDisplay();
         actorGroup.addActor(display);
-        isOccupied = new boolean[GameConstants.GRID_HEIGHT_TILE][GameConstants.GRID_WIDTH_TILE];
+        isOccupied = new int[GameConstants.GRID_HEIGHT_TILE][GameConstants.GRID_WIDTH_TILE];
+        
+        for (int y = 0; y < GameConstants.GRID_HEIGHT_TILE; y++) {
+            for (int x = 0; x < GameConstants.GRID_WIDTH_TILE; x++) {
+                isOccupied[y][x] = NO_ROOM;
+            }
+        }
     }
     
     public RoomGridDisplay getDisplay() {
@@ -27,17 +41,54 @@ public class RoomGrid {
     public void addRoom(Room room) {
         rooms.add(room);
         actorGroup.addActor(room);
+        int roomIndex = rooms.size() - 1;
         
         for (int x = 0; x < room.getTileWidth(); x++) {
             for (int y = 0; y < room.getTileHeight(); y++) {
                 if (room.isPartOfRoom(x, y)) {
-                    isOccupied[room.getTileY() + y][room.getTileX() + x] = true;
+                    isOccupied[room.getTileY() + y][room.getTileX() + x] = roomIndex;
                 }
             }
+        }
+        
+        recalculateAdjacencies();
+    }
+    
+    private void recalculateAdjacencies() {
+        adjacencies.clear();
+        for (int i = 0; i < rooms.size(); i++) {
+            adjacencies.put(i, new HashSet<Integer>());
+        }
+        for (int y = 0; y < GameConstants.GRID_HEIGHT_TILE; y++) {
+            for (int x = 0; x + 1 < GameConstants.GRID_WIDTH_TILE; x++) {
+                int currentRoom = isOccupied[y][x];
+                int nextRoom = isOccupied[y][x+1];
+                if (currentRoom == nextRoom) continue;
+                if (currentRoom == NO_ROOM || nextRoom == NO_ROOM) continue;
+                
+                adjacencies.get(currentRoom).add(nextRoom);
+                adjacencies.get(nextRoom).add(currentRoom);
+            }
+        }
+        
+        for (int y = 0; y + 1 < GameConstants.GRID_HEIGHT_TILE; y++) {
+            for (int x = 0; x < GameConstants.GRID_WIDTH_TILE; x++) {
+                int currentRoom = isOccupied[y][x];
+                int nextRoom = isOccupied[y+1][x];
+                if (currentRoom == nextRoom) continue;
+                if (currentRoom == NO_ROOM || nextRoom == NO_ROOM) continue;
+                
+                adjacencies.get(currentRoom).add(nextRoom);
+                adjacencies.get(nextRoom).add(currentRoom);
+            }
+        }
+        
+        for (int i = 0; i < rooms.size(); i++) {
+            System.out.println("Room #" + i + " is adjacent to " + adjacencies.get(i).stream().map(Object::toString).collect(Collectors.joining(",")));
         }
     }
 
     public boolean isOccupied(int x, int y) {
-        return isOccupied[y][x];
+        return isOccupied[y][x] != NO_ROOM;
     }
 }
