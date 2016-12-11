305c3ea7e0545f024c10c4ab6e8a658a4af3e01a
Harald Krämer <tetha42@gmail.com>
2016-12-11 09:58:22 +0100

    forgot to commit this build engine action

---
+++ core/src/org/subquark/tetris_station/cards/actions/CardAction.java
@@ -0,0 +1,7 @@
+package org.subquark.tetris_station.cards.actions;
+
+import org.subquark.tetris_station.build_overlay.BuildOverlay;
+
+public interface CardAction {
+    public void run(BuildOverlay buildOverlay);
+}
